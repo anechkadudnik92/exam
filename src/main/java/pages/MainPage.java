28394a06d071bfbac9daeb938b630be3dff93e09
Anna <anechkadudnik92@gmail.com>
2016-11-29 08:32:59 +0200

    fix

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -12,14 +12,15 @@ import java.util.List;
 public class MainPage {
     private WebDriver webDriver;
     private WebDriverWait wait;
-    List<WebElement> bugs;
-    WebElement button;
-    String notes;
-    String priority;
-    Actions action;
+    private List<WebElement> bugs;
+    private WebElement button;
+    private String notes;
+    private String priority;
+    private Actions action;
 
     public MainPage(WebDriver driver){
         webDriver = driver;
+        wait = new WebDriverWait(webDriver, 30);
     }
 
     public WebElement findBugByName(String name){
@@ -66,29 +67,37 @@ public class MainPage {
 
     public void addName(String myname){
         action = new Actions(webDriver);
+        bugs = webDriver.findElement(By.cssSelector("table[class='x-grid-table x-grid-table-resizer']")).findElements(By.tagName("tr"));
         WebElement name = bugs.get(bugs.size()-1).findElements(By.tagName("td")).get(2).findElement(By.tagName("div"));
         action.doubleClick(name).perform();
-        webDriver.findElement(By.cssSelector("div[class='x-panel-header x-panel-header-default x-horizontal x-panel-header-horizontal x-panel-header-default-horizontal x-top x-panel-header-top x-panel-header-default-top x-unselectable x-docked x-docked-top x-panel-header-docked-top x-panel-header-default-docked-top']"))
-        .findElements(By.cssSelector("div[class='x-editor x-small-editor x-grid-editor x-editor-default x-layer']")).get(1)
+        webDriver.findElements(By.cssSelector("div[class='x-editor x-small-editor x-grid-editor x-editor-default x-layer']")).get(0)
                 .findElement(By.tagName("input")).sendKeys(myname, Keys.ENTER);
     }
 
     public void addNote(String mynote){
         action = new Actions(webDriver);
+        bugs = webDriver.findElement(By.cssSelector("table[class='x-grid-table x-grid-table-resizer']")).findElements(By.tagName("tr"));
         WebElement note = bugs.get(bugs.size()-1).findElements(By.tagName("td")).get(3).findElement(By.tagName("div"));
         action.doubleClick(note).perform();
-        webDriver.findElement(By.cssSelector("div[class='x-panel-header x-panel-header-default x-horizontal x-panel-header-horizontal x-panel-header-default-horizontal x-top x-panel-header-top x-panel-header-default-top x-unselectable x-docked x-docked-top x-panel-header-docked-top x-panel-header-default-docked-top']"))
-                .findElements(By.cssSelector("div[class='x-editor x-small-editor x-grid-editor x-editor-default x-layer']")).get(2)
-                .findElement(By.tagName("textarea")).sendKeys(mynote);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div[class='x-editor x-small-editor x-grid-editor x-editor-default x-layer']")));
+        webDriver.findElements(By.cssSelector("div[class='x-editor x-small-editor x-grid-editor x-editor-default x-layer']")).get(1)
+                        .findElement(By.tagName("textarea")).sendKeys(mynote);
     }
 
     public void addPriority(String mypriority){
         action = new Actions(webDriver);
-        WebElement note = bugs.get(bugs.size()-1).findElements(By.tagName("td")).get(4).findElement(By.tagName("div"));
-        action.doubleClick(note);
-        webDriver.findElement(By.cssSelector("div[class='x-panel-header x-panel-header-default x-horizontal x-panel-header-horizontal x-panel-header-default-horizontal x-top x-panel-header-top x-panel-header-default-top x-unselectable x-docked x-docked-top x-panel-header-docked-top x-panel-header-default-docked-top']"))
-                .findElements(By.cssSelector("div[class='x-editor x-small-editor x-grid-editor x-editor-default x-layer']")).get(3)
-                .findElement(By.tagName("input")).sendKeys(mypriority);
+        bugs = webDriver.findElement(By.cssSelector("table[class='x-grid-table x-grid-table-resizer']")).findElements(By.tagName("tr"));
+        WebElement priority = bugs.get(bugs.size()-1).findElements(By.tagName("td")).get(4).findElement(By.tagName("div"));
+        action.doubleClick(priority);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div[class='x-field x-form-item x-field-default x-form-dirty']")));
+        webDriver.findElement(By.cssSelector("div[class='x-field x-form-item x-field-default x-form-dirty']"))
+                .findElement(By.tagName("input")).sendKeys(mypriority, Keys.ENTER);
+    }
+
+    public WebElement apply(){
+        WebElement button =  webDriver.findElements(By.cssSelector("div[class='x-btn x-box-item x-toolbar-item x-btn-default-toolbar-small x-noicon x-btn-noicon x-btn-default-toolbar-small-noicon']"))
+                .get(1).findElement(By.tagName("span"));
+        return button;
     }
 
     public void deleteBug(WebElement bug){
@@ -96,13 +105,15 @@ public class MainPage {
         try {
             webDriver.findElements(By.cssSelector("div[class='x-btn x-box-item x-toolbar-item x-btn-default-toolbar-small x-noicon x-btn-noicon x-btn-default-toolbar-small-noicon']"))
                     .get(3).findElement(By.tagName("span")).click();
-            Alert alert = webDriver.switchTo().alert();
-            alert.accept();
-
         }
         catch (Exception ex){
             throw new RuntimeException("Button 'Delete' was not found");
         }
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div[class='x-box-inner']")));
+        webDriver.findElement(By.cssSelector("div[class='x-toolbar x-window-item x-toolbar-footer x-docked x-docked-bottom x-toolbar-docked-bottom x-toolbar-footer-docked-bottom x-box-layout-ct']"))
+                .findElement(By.cssSelector("div[class='x-box-inner']"))
+                .findElements(By.cssSelector("div[class='x-btn x-box-item x-toolbar-item x-btn-default-small x-noicon x-btn-noicon x-btn-default-small-noicon']"))
+                .get(1).findElement(By.tagName("button")).click();
 
     }
 
@@ -111,13 +122,16 @@ public class MainPage {
                 .get(2).findElement(By.tagName("span"));
         return buttton;
     }
-
     public void fillInDialog(String name, String note, String priority){
-        Alert alert = webDriver.switchTo().alert();
-        webDriver.findElement(By.cssSelector("input[name = 'name']")).sendKeys(name);
-        webDriver.findElement(By.cssSelector("textarea[name = 'note']")).sendKeys(note);
-        webDriver.findElement(By.cssSelector("input[name = 'priority']")).sendKeys(priority);
-        alert.accept();
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("span[class='x-window-header-text x-window-header-text-default']")));
+        webDriver.findElement(By.name("name")).sendKeys(name);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("notes")));
+        webDriver.findElement(By.name("notes")).sendKeys(note);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.name("priority")));
+        webDriver.findElement(By.name("priority")).sendKeys(priority);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("button[data-qtip='OK']")));
+        webDriver.findElement(By.cssSelector("button[data-qtip='OK']")).click();
+
     }
 
 }
--- src/test/java/MainPageTests.java
+++ src/test/java/MainPageTests.java
@@ -2,11 +2,13 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.List;
@@ -18,19 +20,14 @@ public class MainPageTests {
 
     @Before
     public void setUp(){
-        //webdriver = new ChromeDriver();
-        webdriver = new FirefoxDriver();
+        webdriver = new ChromeDriver();
+        //webdriver = new FirefoxDriver();
         webdriver.get("http://autotesttask.herokuapp.com/");
         wait = new WebDriverWait(webdriver,30);
 
         app = new App(webdriver);
     }
-
-//1. First test is present
-    //2.Add bug
-    //3. Add in form
-    //4. Added bug is present
-    //5. Delete bug
+    
     @Test
     public void findFirstTest(){
        WebElement firstBug = app.mainPage().findBugByName("First bug");
@@ -41,23 +38,29 @@ public class MainPageTests {
     @Test
     public void addBugTest(){
         app.mainPage().addButton().click();
-        app.mainPage().addName("myname"); //Doesn't work
+        app.mainPage().addName("myname");
         app.mainPage().addNote("MyNote");
         app.mainPage().addPriority("3");
+        app.mainPage().apply();
+        WebElement newbug = app.mainPage().findBugByName("myname");
+        app.mainPage().deleteBug(newbug);
+    }
+
+
+    @Test
+    public void addDialogTest(){
+        app.mainPage().addDialog().click();
+        app.mainPage().fillInDialog("myname1", "note", "3");
+        WebElement newbug = app.mainPage().findBugByName("myname1");
+        app.mainPage().deleteBug(newbug);
     }
 
     @Test
     public void deleteBugTest(){
         WebElement bug = app.mainPage().findBugByName("myname");
-                app.mainPage().deleteBug(bug);  //Doesn't work
-    }
+        app.mainPage().deleteBug(bug);
 
-    @Test
-    public void addDialogTest(){
-        app.mainPage().addDialog();
-        app.mainPage().fillInDialog("myname1", "note", "proirity"); //Doesn't work
     }
-
 
     @After
     public void quit(){
